982aad68a497c7d67b03b360b6d81e61a1f2fa78
Matheus Costa <matheus.costa@proansi.com.br>
2020-12-23 16:50:21 -0300

    create-react-app

---
+++ react-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Fala galera! Essa é a minha primeira aplicação em React!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
